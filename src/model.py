06517232c6c909b7b1a4c61f285d8511d26f0656
XenBond <xeonbond@gmail.com>
2025-04-01 21:55:03 -0500

    change var name

--- src/model.py
+++ src/model.py
@@ -15,13 +15,13 @@ class SquareModel(nn.Module):
         self.pool = nn.MaxPool2d(2, 2) # output size: 32 * 29 * 29
         self.fc1 = nn.Linear(32 * 29 * 29, 128)
         self.bn3 = nn.BatchNorm1d(128)
-        self.fc3 = nn.Linear(128, 3)
+        self.fc2 = nn.Linear(128, 3)
 
     def forward(self, x):
         x = self.pool(F.relu(self.bn1(self.conv1(x))))
         x = self.pool(F.relu(self.bn2(self.conv2(x))))
         x = x.view(-1, 32 * 29 * 29)
         x = F.relu(self.bn3(self.fc1(x)))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
